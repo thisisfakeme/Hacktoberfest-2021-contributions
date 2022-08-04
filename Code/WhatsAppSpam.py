60e97d36d2a7a1d39276a03315cd24647ee906e2
Streetlight321 <98481263+Streetlight321@users.noreply.github.com>
2022-08-04 13:15:44 +0300

    Update WhatsAppSpam.py

--- Code/WhatsAppSpam.py
+++ Code/WhatsAppSpam.py
@@ -1,15 +1,12 @@
+
 import pyautogui
 import webbrowser as wb
 import time
+
 wb.open("web.whatsapp.com")
+
 time.sleep(30)
-for i in range(40):
-    pyautogui.press("H")
-    pyautogui.press("E")
-    pyautogui.press("Y")
-    pyautogui.press("Y")
-    pyautogui.press("O")
-    pyautogui.press("U")
-    pyautogui.press("!")
+for i in range(40): #   Loops through fourty times
+    pyautogui.write("HEYYOU!") #    Small optimization that, instead of writing "HEYYOU!" one letter at a time, it writes it all at once.
     pyautogui.press("enter")
-#scan whatsapp from the appeared window and open the chat where you want to spam.
+#scan whatsapp from the appeared window and open the chat where you want to spam. You only have a mere 40 seconds to do so.
